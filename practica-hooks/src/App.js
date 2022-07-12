47f7c7e4f8c0663e6e96cafddc7f9535ee3fa4e0
BertaVR <bverges@cifpfbmoll.eu>
2022-07-12 14:26:03 +0200

    feat: hecho ejercicio 1 de hooks

---
+++ practica-hooks/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import EjemploUseState from './components/hooks/Ejemplo1';
+
+function App() {
+  return (
+    <div className="App">
+          <h1 class="ejercicio">Ejercicio 1</h1>
+          <EjemploUseState/>
+          <hr/>
+    </div>
+  );
+}
+
+export default App;
---
+++ practica-hooks/src/components/hooks/Ejemplo1.js
@@ -0,0 +1,58 @@
+// Ejemplo de componente que hace uso del hook useState()
+import React, { useState } from 'react';
+
+function EjemploUseState() {
+
+
+    // Valor inicial para un contador
+
+    const valorInicial = 0;
+
+    // Valor inicial para un usuario
+
+    const usuarioInicial ={
+        nombre: 'Berta',
+        email: 'bertaverges@gmail.com'
+    }
+
+    // Sintaxis e inicialización de useState
+    var  [contador, setContador] = useState(valorInicial);
+    const [usuario, setUsuario] = useState(usuarioInicial) ;
+
+    /* A partir de ahora vamos a hacer uso de setContador y setUsuario 
+    ** para modificar el estado (valor) del contador y del usuario respectivamente
+    */
+
+    function incrementar(){ // CLOSURE
+        // aumentar el contador en 1
+        setContador(++contador)
+        console.log('Contador actual', contador)
+    }
+
+    function actualizarUsuario(){
+
+        //Hacer uso de setUsuario para actualizarlo
+        const nuevoUsuario = {
+            nombre: 'Pepito',
+            email: 'pepito@gmail.com'
+        }
+        setUsuario(nuevoUsuario)
+
+    }
+    return (
+        <div>
+            <h2> ***** EJEMPLO HOOK USESTATE() *****</h2>
+            <h3> Contador {contador}</h3>
+            <h3> Datos del usuario:</h3>
+            <h4>Nombre:{usuario.nombre}</h4>
+            <h4>Email: {usuario.email}</h4>
+
+            {/*Evento onclick que llama a las funciones*/}
+            <button onClick={incrementar}> Incrementar Contador</button>
+            <button onClick={actualizarUsuario}> Actualizar Usuario</button>
+
+        </div>
+    );
+}
+
+export default EjemploUseState;
