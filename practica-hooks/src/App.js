29c84010796da75cb8df6add588287128b2822ae
BertaVR <bverges@cifpfbmoll.eu>
2022-07-13 08:51:14 +0200

    feat: ejemplo 3 hooks

--- practica-hooks/src/App.js
+++ practica-hooks/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import EjemploUseState from './components/hooks/Ejemplo1';
 import EjemploOtrosHooks from './components/hooks/Ejemplo2';
+import EjemploUseContext from './components/hooks/Ejemplo3';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
           <hr/>
           <h1 class="ejercicio">Ejercicio 2</h1>
           <EjemploOtrosHooks/>
+          <hr/>
+          <h1 class="ejercicio">Ejercicio 3</h1>
+          <EjemploUseContext/>
           <hr/>
     </div>
   );
