d4527f9d24a510e7a5d634f8ec8bfbc9244cc0e6
BertaVR <bverges@cifpfbmoll.eu>
2022-07-14 09:11:45 +0200

    feat: primer ejercicio eventos

---
+++ eventos/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import LoginControl from "./components/LogIn/LoginControl";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Ejercicio 1 (componentes children)</h1>
+      <LoginControl />
+      <hr />
+    </div>
+  );
+}
+
+export default App;
