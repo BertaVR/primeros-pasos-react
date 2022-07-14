34f84b648cdce8316540620dd2bcabb47c5f1da0
BertaVR <bverges@cifpfbmoll.eu>
2022-07-14 10:14:21 +0200

    refactor: ejercicio de lols mensajes pasado a op. ternario

--- eventos/src/components/Mensajes.js
+++ eventos/src/components/Mensajes.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export default function Mensajes(props) {
-    const mensajesSinLeer = props.mensajesSinLeer;
-        
-    return (
-        <div>
-         {mensajesSinLeer > 0 &&
+  const mensajesSinLeer = props.mensajesSinLeer;
+
+  return (
+    <div>
+      {/*mensajesSinLeer > 0 &&
             <p> Tienes {mensajesSinLeer} mensajes sin leer... </p>
             /* La segunda condición siempre es true porque
              una cadena de texto no vacía es Truthy*/}
-        </div>
-    );
+
+      {mensajesSinLeer > 0 ? (
+        <p> Tienes {mensajesSinLeer} mensajes sin leer... </p>
+      ) : (
+        <p> No tienes mensajes pendientes... </p>
+      )}
+    </div>
+  );
 }
