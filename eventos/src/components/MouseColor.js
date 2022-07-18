e206c47d6ba5b9749782ea6dec79957fcc70f3c2
BertaVR <bverges@cifpfbmoll.eu>
2022-07-18 08:11:42 +0200

    quitada propiedad stop color

--- eventos/src/components/MouseColor.js
+++ eventos/src/components/MouseColor.js
@@ -3,20 +3,17 @@ import "./MouseColor.css";
 
 export default function MouseColor() {
   const colorInicial = { red: 123, green: 234, blue: 221 };
-  const stopColorInicial = false;
 
-  const [stopColor, setStopColor] = useState(stopColorInicial);
   const [color, setColor] = useState(colorInicial);
 
   const manejoMovimientoRaton = (e) => {
     e.preventDefault();
-    if (!stopColor) {
       setColor({
         red: e.clientX,
         green: e.clientY,
         blue: e.clientX,
       });
-    }
+    
     console.log(color);
   };
   return (
