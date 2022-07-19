fd0476291a1d9353c9aab61c55509b95722acd4e
BertaVR <bverges@cifpfbmoll.eu>
2022-07-19 12:48:38 +0200

    fix:  sintaxis toggle todo, ahora funciona

--- redux/src/store/reducers/todosReducer.js
+++ redux/src/store/reducers/todosReducer.js
@@ -1,28 +1,34 @@
-let initialState = [];
+let initialState = [{id:0, text: 'Todo de prueba', completed:false}];
 
 
+const todosReducer = (state = initialState, action) => {
 
-const todosReducer = (state = [], action) => {
   switch (action.type) {
-    case "ADD_TODO":
-      return [
-        ...state,
-        {
-          id: action.payload.id,
-          text: action.payload.text,
-          completed: false,
-        },
-      ];
-    case "TOGGLE-TODO":
-      return state.map((todo) =>
-        todo.id === action.payload.id
-          ? { ...todo, completed: !todo.completed }
-          : todo
-      );
-    default:
-      return state;
+      case 'ADD_TODO':
+          return [
+              ...state,
+              {
+                  id: action.payload.id,
+                  text: action.payload.text,
+                  completed: false
+              }
+          ]
+      case 'TOGGLE_TODO':
+          return state.map( todo => 
+              (todo.id === action.payload.id)
+              ? 
+                  {
+                      ...todo,
+                      completed: !todo.completed
+                  }
+              :
+                  todo
+          );
+          
+  
+      default:
+          return state;
   }
-};
-
+} 
 
 export default todosReducer;
