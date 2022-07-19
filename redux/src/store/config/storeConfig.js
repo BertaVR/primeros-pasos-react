bc464f557f7e7bb0fff8315e35b3fa498dc4fcfe
BertaVR <bverges@cifpfbmoll.eu>
2022-07-19 12:48:12 +0200

    fix: cambiado indexJs  usando createStore. TODO: cambiar (deprecado)

--- redux/src/store/config/storeConfig.js
+++ redux/src/store/config/storeConfig.js
@@ -1,12 +1,13 @@
 // Función para crear el Store de la aplicación
-import { configureStore } from "redux";
+import { createStore } from "redux";
 // Importamos el Root Reducer para dárselo al store de la aplicación
 import rootReducer from "../reducers/rootReducer";
 
 // Importamos ComposeWithDevTools para poder analizar el store de la app en el navegador
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = configureStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
+// *TODO create store está deprecado, usar configureStore de @reduxjs/toolkit
 
 export default store;
